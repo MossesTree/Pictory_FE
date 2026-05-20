bc508c3e291bce88e9ebc2fe6b1969717ab068f0
MossesTree <lighttreekim@gmail.com>
2026-05-20 10:18:51 +0900

    test: add splash screen widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:picktory/app/di/service_locator.dart';
+import 'package:picktory/app/picktory_app.dart';
+import 'package:picktory/viewmodels/splash_view_model.dart';
+
+void main() {
+  setUp(() {
+    ServiceLocator.instance.init();
+  });
+
+  testWidgets('Splash screen shows brand and loading',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(const PicktoryApp());
+    await tester.pump();
+
+    expect(find.text('픽토리'), findsOneWidget);
+    expect(find.text('로딩 중...'), findsOneWidget);
+
+    await tester.pump(SplashViewModel.maxWait);
+    await tester.pumpAndSettle();
+  });
+}
